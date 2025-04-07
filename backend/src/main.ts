6c21d6d800f1213904f41d0ed23cd714a17c3f56
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-07 16:55:35 +0300

    Added config module and setup the app

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService);
+  app.setGlobalPrefix('api/v1');
+  app.use(cookieParser());
+  app.enableCors({
+    origin: configService.get<string>("YEMENCAREERS_APP_FRONTEND_URL"),
+    credentials: true
+  })
+  const logger = new Logger("bootstrap", {timestamp: true});
+  const PORT = Number.parseInt(configService.get<string>("YEMENCAREERS_APP_PROT"));
+  await app.listen(PORT, () => logger.log(`App running on prot: ${PORT}`) );
 }
 bootstrap();

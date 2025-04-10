71f174210faafc0e528fae7bcb1d5ccd1b47099b
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-10 22:35:20 +0300

    Added register logic

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,13 +4,17 @@ import { AuthModule } from 'src/auth/auth.module';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { UserController } from './user.controller';
 import { UserEntity } from './entities/user.entity';
+import { UserService } from './user.service';
+import { MulterModule } from '@nestjs/platform-express';
+import { multerConfig } from 'src/config/multer.config';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([UserEntity]),
+        MulterModule.register(multerConfig),
         AuthModule
     ],
-    providers: [JwtStrategy],
+    providers: [JwtStrategy, UserService],
     controllers: [UserController]
 })
 export class UserModule {}

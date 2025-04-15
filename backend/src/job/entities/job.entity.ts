f85964da2f0052ba1a7f2deb114a8de47cbff245
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-15 21:24:26 +0300

    Added job entity

---
+++ backend/src/job/entities/job.entity.ts
@@ -0,0 +1,53 @@
+import { 
+    BaseEntity,
+    Column,
+    Entity,
+    ManyToOne,
+    PrimaryGeneratedColumn, 
+    UpdateDateColumn
+} from "typeorm";
+import { JobStatus } from "../enums/job-status.enum";
+import { UserEntity } from "src/user/entities/user.entity";
+
+
+@Entity({name: 'jobs'})
+export class JobEntity extends BaseEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({length: 255})
+    title: string;
+
+    @Column({type: 'text'})
+    body: string;
+
+    @Column({length: 100})
+    type: string;
+
+    @Column({length: 100})
+    department: string;
+
+    @Column({type: 'timestamp'})
+    end_date: Date;
+
+    @Column({length: 100})
+    city: string;
+
+    @Column({enum: JobStatus, default: JobStatus.OPEN})
+    status: JobStatus;
+
+    @Column()
+    apply_url: string;
+
+    @Column()
+    user_id: number;
+
+    @ManyToOne((_) => UserEntity, (user) => user.jobs)
+    user: UserEntity;
+
+    @Column({type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date
+}

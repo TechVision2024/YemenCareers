43b2f191adce0e544702fde468e349c683c40675
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-15 21:54:42 +0300

    Added job controller

--- backend/src/job/job.module.ts
+++ backend/src/job/job.module.ts
@@ -4,12 +4,14 @@ import { AuthModule } from 'src/auth/auth.module';
 import { JwtStrategy } from 'src/auth/jwt.strategy';
 import { JobEntity } from './entities/job.entity';
 import { UserEntity } from 'src/user/entities/user.entity';
+import { JobController } from './job.controller';
 
 @Module({
     imports: [
         TypeOrmModule.forFeature([JobEntity, UserEntity]),
         AuthModule
     ],
-    providers: [JwtStrategy]
+    providers: [JwtStrategy],
+    controllers: [JobController]
 })
 export class JobModule {}

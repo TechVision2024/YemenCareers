167d157593be5e64db71b1e1f83df8596593e0a5
Mohaned Mohammed Sherhan <shrhanmhnd15@gmail.com>
2025-04-17 20:32:38 +0300

    Added get job information logic

---
+++ backend/src/job/interfaces.ts
@@ -0,0 +1,17 @@
+import { JobStatus } from "./enums/job-status.enum";
+
+export interface JobInformaionInterface {
+    title: string;
+    body: string;
+    type: string;
+    department: string;
+    end_date: Date;
+    created_at: Date;
+    updated_at: Date;
+    city: string;
+    apply_url: string;
+    compnay_image: string;
+    compnay_name: string;
+    remaining_days: number;
+    state: JobStatus
+}
--- backend/src/job/job.service.ts
+++ backend/src/job/job.service.ts
@@ -1,10 +1,16 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { 
+    Injectable, 
+    Logger, 
+    NotFoundException 
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { JobEntity } from './entities/job.entity';
 import { Repository } from 'typeorm';
 import { CreateJobDto } from './dtos/create.dto';
 import { UserEntity } from 'src/user/entities/user.entity';
 import { omitObjectKeys } from 'src/utils/omit.util';
+import { JobInformaionInterface } from './interfaces';
+import { JobStatus } from './enums/job-status.enum';
 
 @Injectable()
 export class JobService {
@@ -19,5 +25,25 @@ export class JobService {
         job.user = user;
         this.logger.log(`${user.name} created new job.`);
         return omitObjectKeys(await job.save(), ['user']) as JobEntity;
+    }
+
+    async jobInofrmation(id: number): Promise<JobInformaionInterface> {
+        const job = await this.jobRepository.findOne({
+            where: {id},
+            relations: ['user']
+        });
+        if (!job) throw new NotFoundException();
+        let jobInfo: JobInformaionInterface = {
+            ...omitObjectKeys(job, ['user', 'userId' ]),
+            compnay_image: job.user.profile_image_url,
+            compnay_name: job.user.name,
+            remaining_days: Math.round(
+                // 1d = 86,400,000ms
+                ( (new Date(job.end_date)).getTime() - (new Date()).getTime() )/86400000
+            ),
+            state: JobStatus.CLOSE
+        };
+        jobInfo.state = (jobInfo.remaining_days <= 0) ? JobStatus.CLOSE : JobStatus.OPEN;
+        return jobInfo;
     }
 }
